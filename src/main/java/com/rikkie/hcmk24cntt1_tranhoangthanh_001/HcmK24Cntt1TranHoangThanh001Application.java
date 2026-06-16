a911bc9c5bb81fbeaea16d5a13a57552bc93ae2a
tran hoang thanh <tranhoangthanh@MacBook-Air-2.local>
2026-06-16 09:20:12 +0700

    first commit

---
+++ src/main/java/com/rikkie/hcmk24cntt1_tranhoangthanh_001/HcmK24Cntt1TranHoangThanh001Application.java
@@ -0,0 +1,13 @@
+package com.rikkie.hcmk24cntt1_tranhoangthanh_001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcmK24Cntt1TranHoangThanh001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HcmK24Cntt1TranHoangThanh001Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/rikkie/hcmk24cntt1_tranhoangthanh_001/HcmK24Cntt1TranHoangThanh001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rikkie.hcmk24cntt1_tranhoangthanh_001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HcmK24Cntt1TranHoangThanh001ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
