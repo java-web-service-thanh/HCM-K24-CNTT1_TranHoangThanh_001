2f492fb1ba9136574db4baede74c48c7910a67b8
tran hoang thanh <tranhoangthanh@MacBook-Air-2.local>
2026-06-16 09:22:22 +0700

    lan2

---
+++ src/main/java/com/rikkie/hcmk24cntt1_tranhoangthanh_001/model/Book.java
@@ -0,0 +1,4 @@
+package com.rikkie.hcmk24cntt1_tranhoangthanh_001.model;
+
+public class Book {
+}
---
+++ src/main/java/com/rikkie/hcmk24cntt1_tranhoangthanh_001/service/BookService.java
@@ -0,0 +1,4 @@
+package com.rikkie.hcmk24cntt1_tranhoangthanh_001.service;
+
+public class BookService {
+}
